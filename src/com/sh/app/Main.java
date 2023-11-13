eee7141ac92d5195b3c87d67734c4f08ed51ed32
seungbeom4382 <jsd940@naver.com>
2023-11-13 11:35:32 +0900

    Monkey#walk 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -3,6 +3,7 @@ package com.sh.app;
 import com.sh.app.animal.Bird;
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
+import com.sh.app.animal.Monkey;
 import com.sh.app.animal.Snake;
 
 public class Main {
@@ -14,5 +15,6 @@ public class Main {
 		new Cat().jump();
 		new Snake().bite();
 		new Bird().fly();
+		new Monkey().walk();
 	}
 }
---
+++ src/com/sh/app/animal/Monkey.java
@@ -0,0 +1,7 @@
+package com.sh.app.animal;
+
+public class Monkey {
+	public void walk() {
+		System.out.println("원숭이 걸어가기!");
+	}
+}
