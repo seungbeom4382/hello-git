d11b5a638b7b375850e5b34d39d6c9d27d268601
seungbeom4382 <jsd940@naver.com>
2023-11-13 11:05:22 +0900

    Snake#bite 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -2,6 +2,7 @@ package com.sh.app;
 
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
+import com.sh.app.animal.Snake;
 
 public class Main {
 	public static void main(String args[]) {
@@ -10,5 +11,6 @@ public class Main {
 		
 		new Dog().bark();
 		new Cat().jump();
+		new Snake().bite();
 	}
 }
---
+++ src/com/sh/app/animal/Snake.java
@@ -0,0 +1,7 @@
+package com.sh.app.animal;
+
+public class Snake {
+	public void bite() {
+		System.out.println("뱀이 물다!");
+	}
+}
