952ca498512ffd9f8c62d3f2828a09d5e61bfe3d
seungbeom4382 <jsd940@naver.com>
2023-11-13 11:21:56 +0900

    Main.main 대문자로 수정

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -6,9 +6,9 @@ import com.sh.app.animal.Snake;
 
 public class Main {
 	public static void main(String args[]) {
-		System.out.println("hello world!");
-		System.out.println("hello Git");
-		
+		System.out.println("HELLO WORLD!");
+		System.out.println("HELLO GIT");
+
 		new Dog().bark();
 		new Cat().jump();
 		new Snake().bite();
