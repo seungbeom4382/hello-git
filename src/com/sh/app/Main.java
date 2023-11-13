c0047b6135ef05d2c075d75948eefd317f24e3f7
seungbeom4382 <jsd940@naver.com>
2023-11-13 10:33:34 +0900

    Dog#bark 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,8 +1,12 @@
 package com.sh.app;
 
+import com.sh.app.animal.Dog;
+
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("hello world!");
 		System.out.println("hello Git");
+		
+		new Dog().bark();
 	}
 }
